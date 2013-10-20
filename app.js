2584d3396d99a699a00131c4bdbd5de2be422fe3
Sarabpreet Singh <sarabpreetsingh23@gmail.com>
2013-10-20 09:47:55 +0530

    settings

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 //var routes = require('./routes');
 //var user = require('./routes/user');
 var http = require('http');
 //var path = require('path');
-
 var app = express();
 
 app.configure(function(){
 	app.set('port',process.env.PORT||3000);
+	app.set('views'),__dirname+'/views');
+	app.set('view engine','jade');
+//    app.set('view cache'),true);
+//    app.ebale('view cache');
 	app.use(express.bodyParser());
 });
 
