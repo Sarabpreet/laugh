10b16db59336295fba79fd755cad9e648d4cc3b7
Sarabpreet Singh <sarabpreetsingh23@gmail.com>
2013-10-25 05:32:05 +0530

    practising jade & maxins

--- app.js
+++ app.js
@@ -11,25 +11,28 @@ var app = express();
 
 app.configure(function(){
 	app.set('port',process.env.PORT||3000);
-	app.set('views'),__dirname+'/views');
+	app.set('views',__dirname + '/views');
 	app.set('view engine','jade');
+
+
 //    app.set('view cache'),true);
 //    app.ebale('view cache');
 	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.static(__dirname + "/public"));
 });
 
 
 
 app.get('/',function(req,res) {
 
-	res.send("Hello Express!");
+	res.render('home',{
+		title:"welcome to laugh.im", para:"this is an example of a variable"
+	});
 });
 
-app.get('/hi',function(req,res){
-var message="<h1>hello there, how you doing today?</h1>";
-res.send(message);
 
-});
+
 
 //app.get('/users/:userId',function(req,res){
 //res.send("<h2>hello,user "+ req.params.userId+" </h2> !");
@@ -39,30 +42,30 @@ res.send(message);
 //app.put /users/:userId
 //app.delete /users/:userId
 
-app.get(/\/users\/(\d*)\/?(edit)?/,function(req,res) {
+// app.get(/\/users\/(\d*)\/?(edit)?/,function(req,res) {
 
 	//users/456
 	//users/456/
 	//user/456/edit
-var message="user "+req.params[0]+"'s profile";
+// var message="user "+req.params[0]+"'s profile";
 
 
-if(req.params[1]==='edit'){
+// if(req.params[1]==='edit'){
 
-	message="Edditing "+req.params[1]+" "+message;
-}
- else {
+	// message="Edditing "+req.params[1]+" "+message;
+// }
+ // else {
 
- 	message=" Viewing" +message;
- }
- res.send(message);
-});
+//  	message=" Viewing" +message;
+//  }
+//  res.send(message);
+// });
 
-app.post("/users",function(req,res){
+// app.post("/users",function(req,res){
 
-res.send("Creating a new user with the name "+ req.body.username);
+// res.send("Creating a new user with the name "+ req.body.username);
 
-});
+// });
 
 /*
 // all environments
